87f1e661714a593e6f571887d5fe12d50cbd766b
Cristian K. Cárdenas <ccardensa@outlook.com>
2022-08-13 12:27:50 -0400

    Conflicts resolved

---
+++ examples/Ex.Curso.CSharp/Delegados.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Examples.Curso;
+
+namespace Examples.Curso
+{
+    public static class Delegados
+    {
+        public delegate int CalcularIvaDelegate(int impuesto, int valor);
+        public delegate int DelegateMultiple(int impuesto, int valor);
+        public delegate void FunctionDelegate();
+
+        public static int Multiplicar(int a, int b)
+        {
+            
+            return a * b;
+        }
+
+        public static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void MultiplicarVoid(int a, int b)
+        {
+           Console.WriteLine(a * b);
+        }
+
+        public static void SumaVoid(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static void MetodoUno()
+        {
+            Console.WriteLine("Método 1");
+        }
+
+        public static void MetodoDos()
+        {
+            Console.WriteLine("Método 2");
+        }
+
+        /// <summary>
+        /// Delegates
+        /// </summary>
+        public static void TestDelegate()
+        {
+            CalcularIvaDelegate sumaDelegate = Suma;
+            Console.WriteLine("Suma: " + sumaDelegate(10, 20));
+
+            CalcularIvaDelegate MultiDelegate = Multiplicar;
+            Console.WriteLine("Multiplicación: " + MultiDelegate(10, 20));
+
+            Console.WriteLine("Delegado multiple");
+            FunctionDelegate Multiple = MetodoUno;
+            Multiple += MetodoDos;
+
+            Multiple();
+
+            Console.WriteLine("Delegado Genericos");
+            Action<int, int> action = SumaVoid;
+            action(10,20);
+
+            action = MultiplicarVoid;
+            action(10, 20);
+
+            Console.WriteLine("Delegado para funciones");
+            //En el delegado Func adicional a los parametros de entrada debemos agregar en el último lugar el parametro de salida (out)
+            Func<int, int, int> Funcion = Suma;
+            Console.WriteLine(Funcion(10,20));
+            Funcion = Multiplicar;
+            Console.WriteLine(Funcion(10, 20));
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ examples/Ex.Curso.CSharp/Ejercicios de Programacion/Ejercicios Basicos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.Curso
+{
+    public class Ejercicios
+    {
+        ///1) Crear 3 variables numéricas con el valor que tu quieras y en otra variable numérica guardar el valor de la suma de las 3 anteriores. Mostrar por consola.
+        ///2) Ingresa por consola tu nombre completo y tu edad y muestra el siguiente mensaje: «Me llamas » <nombre> » y tengo » <años> » años»
+        ///3) Ingresa 5 números y muestra su promedio
+        ///4) Escribir una funcion que te permita ordenar una lista de 5 números de mayor a menor (puedes usar una lista o un arreglo)
+        ///5) Mostrar números impares entre el 0 y el 300
+        ///6) Mostrar números pares entre el 0 y el 300
+        ///7) Mostrar números entre el 0 y el 200
+        ///8) Mostrar la cantidad de vocales que tiene una frase, la frase debe ser capturada desde el teclado
+    }
+}
