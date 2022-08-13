a7dc6a6f52a185430bd0dbc020b8edb0e0292ee9
Cristian K. Cárdenas <ccardensa@outlook.com>
2022-08-13 12:09:48 -0400

    Add files via upload

---
+++ Examples.Curso.CSharp/Ex.Curso.CSharp/Hola.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.Curso
+{
+    public static class Hola
+    {
+        public static int MultiplicarHola(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Examples.Curso.CSharp/Ex.Curso.CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Examples.Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Delegados.TestDelegate();
+            
+        }
+    }
+}
